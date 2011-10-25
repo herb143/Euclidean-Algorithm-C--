fd31338f6e35e483db75fbdcc68748c215d583cd
herb143 <h.herbie@gmail.com>
2011-10-25 17:28:42 -0400

    Initial Commit

---
+++ EuclideanAlgorithm/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  EuclideanAlgorithm
+//
+//  Created by Herbert Gilman on 10/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
