a33e0097750f55ee1724cf703f4b41dea559208e
herb143 <h.herbie@gmail.com>
2011-10-26 19:14:02 -0400

    Moved test functions to a new file.

---
+++ EuclideanAlgorithm/testFunctions.cpp
@@ -0,0 +1,48 @@
+/*
+ *  testFunctions.cpp
+ *  EuclideanAlgorithm
+ *
+ *  testGCD creates a GCD and checks whether or not it divides in correctly.
+ *  testLoop loops through this 100 million times, and times it.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include "numberFunctions.h"
+#include "testFunctions.h"
+
+void testGCD(int iii, int bbb) // Test function.
+{
+	int nGCDV = nGCD(iii, bbb); // Find GCD.
+	int nGCDWorks = iii / nGCDV;
+	int nGCDWorks2 = bbb / nGCDV;
+	if ((nGCDWorks2 * nGCDV != bbb) || (nGCDWorks * nGCDV != iii)) { // Verify GCD goes evenly into both.
+		std::cerr << "Error! GCD of " << iii << " and " << bbb << " was computed as " << nGCDV << std::endl; exit(1); }
+	
+}
+
+void testLoop() // Loops through the test function for every number combination 1 - 10000
+{
+    time_t start,end;
+    long double count = 0;
+    std::cout << "Testing..." << std::endl;
+    time(&start);
+	for (int iii = 1; iii <= 10000; iii++) {
+		for (int bbb = 1; bbb <= 10000; bbb++)
+		{
+			testGCD(iii, bbb); // NOTE: Since this function is not verbose, it is not truly representative of the speed.
+            // Non-verbose GCD calculations are 99% faster, but this difference (1/500 of a percent of
+            // a second) is so negligible that, since verbose calculations are not used for mass computation,
+            // the effective speed is pretty much the same.
+            count++;
+		}
+	}
+    time(&end);
+    int timediff = difftime(end, start);
+    long double secondsPerCalc = timediff / count;
+	// If the program didn't exit.
+    std::cout << std::setprecision(16);
+	std::cout << "All " << count << " tests completed sucessfully in " << timediff << " seconds." << std::endl;
+    std::cout << "Each GCD took about " << secondsPerCalc << " seconds." << std::endl;
+}
---
+++ EuclideanAlgorithm/testFunctions.h
@@ -0,0 +1,16 @@
+/*
+ *  testFunctions.h
+ *  EuclideanAlgorithm
+ *
+ *  testGCD creates a GCD and checks whether or not it divides in correctly.
+ *  testLoop loops through this 100 million times, and times it.
+ */
+
+#ifndef EuclideanAlgorithm_testFunctions_h
+#define EuclideanAlgorithm_testFunctions_h
+
+void testGCD(int iii, int bbb);
+void testLoop();
+
+
+#endif
