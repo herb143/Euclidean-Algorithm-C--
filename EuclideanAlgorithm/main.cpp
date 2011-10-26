b1dbfd049bb1891a380a1ea3654b203d383b0855
herb143 <h.herbie@gmail.com>
2011-10-26 18:44:48 -0400

    Moved the argument functions to a new file.

--- EuclideanAlgorithm/main.cpp
+++ EuclideanAlgorithm/main.cpp
@@ -23,49 +23,7 @@
 #include <stdlib.h>
 #include <ctime>
 #include <iomanip>
-#include <cmath>
-
-// Begin argument functions.
-enum Command
-{
-	UNKNOWN_COMMAND,
-	COMMAND_GCD,
-	COMMAND_TEST
-};
-
-bool argMatch(int argc, char * const argv[], int argNum, std::string argTarget)
-{
-	if (argc > argNum)
-	{
-		if (argv[argNum] == argTarget) { return true; }
-	}
-	return false; // Otherwise return false.
-}
-
-bool argIsNum(int argc, char * const argv[], int argNum)
-{
-    if (argc > argNum) // If there's enough.
-    {
-        int nValue = atoi(argv[argNum]);
-        if (nValue == 0) { return false; } // Zero value means invalid or... zero... which we can't use.
-        if (nValue == INT_MAX || nValue == INT_MIN) { return false; } // Out of range.
-        else { return true; }
-    }
-    else { return false; }
-}
-
-Command argCheck(int argc, char * const argv[])
-{
-	if (argc == 1) { return UNKNOWN_COMMAND; }
-    else if (argc == 2) { if (argMatch(argc, argv, 1, "--test")) { return COMMAND_TEST; } } // If we have 1 arg and it's test, then test.
-	else if (argc == 4) // If we have 3 arguments.
-    {    
-        if (argMatch(argc, argv, 1, "--GCD") && argIsNum(argc, argv, 2) && argIsNum(argc, argv, 3)) { return COMMAND_GCD; }
-    }
-	else {return UNKNOWN_COMMAND; }
-}
-
-// End arg functions.
+#include "argFunctions.h"
 
 int nGreater(int nA, int nB) // Function for finding the greater of two numbers.
 {
