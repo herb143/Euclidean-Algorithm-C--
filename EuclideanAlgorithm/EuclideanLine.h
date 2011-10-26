439889f841059338f6ac2ffad6e02c9452f8d4ab
herb143 <h.herbie@gmail.com>
2011-10-26 19:55:06 -0400

    New class: EuclideanLine

---
+++ EuclideanAlgorithm/EuclideanLine.h
@@ -0,0 +1,32 @@
+/*
+ *
+ *  EuclideanLine.cpp
+ *  EuclideanAlgorithm
+ *
+ *  Represents one line while working with the Euclidean algorithm.
+ *
+ */
+
+#ifndef EuclideanAlgorithm_EuclideanLine_h
+#define EuclideanAlgorithm_EuclideanLine_h
+
+class EuclideanLine
+{
+private:
+    int m_nDividend;
+    int m_nDiviser;
+    int m_nQuotient;
+    int m_nRemainder;
+public:
+    EuclideanLine(); // Default constructor.
+    EuclideanLine(int nDividend, int nDiviser); // Proper constructor.
+    
+    // Getters.
+    int getDividend() { return m_nDividend; }
+    int getDiviser() { return m_nDiviser; }
+    int getQuotient() { return m_nQuotient; }
+    int getRemainder() { return m_nRemainder; }
+    
+};
+
+#endif
