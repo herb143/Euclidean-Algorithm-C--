de12d6b185fca79bfa7a51742b7a11aeeb53895c
herb143 <h.herbie@gmail.com>
2011-10-27 09:23:52 -0400

    Added extended algorithm class

---
+++ EuclideanAlgorithm/extendedCalculation.cpp
@@ -0,0 +1,14 @@
+/*
+ *
+ *  extendedCalculation.cpp
+ *  EuclideanAlgorithm
+ *
+ *  Represents the calculation of the Extended Euclidean Algorithm.
+ *
+ */
+
+#include <iostream>
+#include "numberFunctions.h"
+#include "EuclideanLine.h"
+#include "gcdCalculation.h"
+#include "extendedCalculation.h"
---
+++ EuclideanAlgorithm/extendedCalculation.h
@@ -0,0 +1,15 @@
+/*
+ *
+ *  extendedCalculation.h
+ *  EuclideanAlgorithm
+ *
+ *  Represents the calculation of the Extended Euclidean Algorithm.
+ *
+ */
+
+#ifndef EuclideanAlgorithm_extendedCalculation_h
+#define EuclideanAlgorithm_extendedCalculation_h
+
+
+
+#endif
